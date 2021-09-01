c6a0d7abadfafa183e104a9dac4db8c7100a0ff1
Neo Shi <shiwenxuan2003@gmail.com>
2021-09-02 04:41:20 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -129,11 +129,11 @@ const links = [
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
-      <title>南开大学软件学院《数据科学》课程平台</title>
+      <title>南开大学《数据科学》课程平台</title>
       <h1 style={headingStyles}>
-        《数据科学》课程资源
+        《数据科学》
         <br />
-        <span style={headingAccentStyles}>南开大学 软件学院 </span>
+        <span style={headingAccentStyles}>— 课程资源 </span>
         <span role="img" aria-label="Party popper emojis">
         🚀 
         </span>
@@ -180,10 +180,8 @@ const IndexPage = () => {
           </li>
         ))}
       </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+      <span>Copyright © 2021 南开大学, 软件学院. Built with Gatsby. </span>
+      
     </main>
   )
 }
