8391105595297f8e898cc0cfc027e2dcc8f23acf
Neo Shi <shiwenxuan2003@gmail.com>
2022-04-11 21:19:52 +0800

    课程内容

--- src/pages/index.js
+++ src/pages/index.js
@@ -143,23 +143,33 @@ const IndexPage = () => {
         </span>
       </h1>
       
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            target={docLink.target}
-            style={linkStyle}
-            href={docLink.url}
-          >
-            {docLink.text}
-          </a>
-        </li></ul>
+      
 
       <p style={paragraphStyles}>
-      
+
+      <code style={codeStyles}>选课名单</code>：
+      <a
+            style={linkStyle}
+            target="_blank"
+            href={"https://docs.qq.com/sheet/DYldualFLZXVKYWtZ"}
+          >
+            腾讯文档
+          </a><br />
+      <br />1. 填写个人邮箱：受邀加入tapd.cn上的项目“智能移动开发_2022春”，用于上机实践中的答疑；
+      <br />2. 填写个人Git项目的访问地址：如github.com、gitLab.com、gitee.com等，用于提交每次的上机实践的成果和学期项目报告。
+      <br /><br /><br />
+
       <code style={codeStyles}>平时成绩（60%）</code><br />
-      <br />1. 将每次上机的成果和徽章截图提交到个人的Git项目中
-      <br />2. 每次上机任务的提交截止时间在下次上机任务之前（<b>晚提交作业会有较重的扣分</b>）
-      <br /><br />
+      <br />1. 课程内容：<a
+            style={linkStyle}
+            target="_blank"
+            href={"https://k.nkugame.com/"}
+          >
+            使用 Kotlin 进行 Android 开发
+          </a>
+      <br />2. 将每次上机的成果和徽章截图提交到个人的Git项目中
+      <br />3. 每次上机任务的提交截止时间在下次上机任务之前（<b>晚提交作业会有较重的扣分</b>）
+      <br /><br /><br />
       <code style={codeStyles}>学期项目报告（40%）</code><br />
       <br />
       1. 参加 
