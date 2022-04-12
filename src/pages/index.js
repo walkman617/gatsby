717a47b2460b55a1051c5e5bdd19d515a5e9549a
Neo Shi <shiwenxuan2003@gmail.com>
2022-04-12 11:03:10 +0800

    change page padding

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,13 @@ import * as React from "react"
 // styles
 const pageStyles = {
   color: "#232129",
-  padding: 96,
+  padding: 60,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
-  maxWidth: 320,
+  maxWidth: 480,
 }
 const headingAccentStyles = {
   color: "#663399",
